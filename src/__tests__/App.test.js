8d5db442b22b672f0414927f33c1d93a6f1e08d3
Alan Chu <alan_chu@yahoo.com>
2019-11-08 14:29:53 +1100

    Test number one: To check all components are present in App.js

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -2,21 +2,43 @@ import React from "react";
 import App from "../App";
 import { shallow } from "enzyme";
 import NavBar from "../components/NavBar";
-
-// import thunk from "redux-thunk";
-// import { Provider } from "react-redux";
-// import { createStore, applyMiddleware } from "redux";
-// import reducers from "../reducers";
-
-// const store = createStore(reducers, applyMiddleware(thunk));
+import TravelPlan from "../components/TravelPlan";
+import TravelCards from "../components/TravelCards";
+import Flash from "../components/Flash";
+import Update from "../components/U_update";
+import Delete from "../components/U_delete";
 
 it("shows a navbar ", () => {
   const wrapped = shallow(<App />);
-
-  // <Provider store={store}>
-  // <App />)
-  // </Provider>
   expect(wrapped.find(NavBar).length).toEqual(1);
+  console.log(Object.keys(wrapped));
+});
 
+it("contains flash box", () => {
+  const wrapped = shallow(<App />);
+  expect(wrapped.find(Flash).length).toEqual(1);
+  console.log(Object.keys(wrapped));
+});
+
+it("contains delete component", () => {
+  const wrapped = shallow(<App />);
+  expect(wrapped.find(Delete).length).toEqual(1);
+  console.log(Object.keys(wrapped));
+});
+
+it("contains update component", () => {
+  const wrapped = shallow(<App />);
+  expect(wrapped.find(Update).length).toEqual(1);
+  console.log(Object.keys(wrapped));
+});
+
+it("contains travel cards component", () => {
+  const wrapped = shallow(<App />);
+  expect(wrapped.find(TravelCards).length).toEqual(1);
+  console.log(Object.keys(wrapped));
+});
+it("contains travel plan component", () => {
+  const wrapped = shallow(<App />);
+  expect(wrapped.find(TravelPlan).length).toEqual(1);
   console.log(Object.keys(wrapped));
 });
