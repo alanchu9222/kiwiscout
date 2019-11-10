ae30b5983e6707be9bae11471278a05fcb4482f4
Alan Chu <alan_chu@yahoo.com>
2019-11-10 11:58:31 +1100

    Updated tests to support the router implementation

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,6 +1,7 @@
 import React from "react";
 import App from "../App";
 import { shallow } from "enzyme";
+import { Switch, Route, Redirect } from "react-router-dom";
 import NavBar from "../components/NavBar";
 import TravelPlan from "../components/TravelPlan";
 import TravelCards from "../components/TravelCards";
@@ -28,9 +29,12 @@ it("contains update component", () => {
   expect(wrapped.find(Update).length).toEqual(1);
 });
 
-it("contains travel cards component", () => {
-  expect(wrapped.find(TravelCards).length).toEqual(1);
+it("contains switch component", () => {
+  expect(wrapped.find(Switch).length).toEqual(1);
 });
-it("contains travel plan component", () => {
-  expect(wrapped.find(TravelPlan).length).toEqual(1);
+it("contains redirect component", () => {
+  expect(wrapped.find(Redirect).length).toEqual(1);
+});
+it("contains route component", () => {
+  expect(wrapped.find(Route).length).toEqual(2);
 });
