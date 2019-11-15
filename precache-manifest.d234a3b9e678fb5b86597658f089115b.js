865443325fbd4ed7a28c562d36092c325e99a36d
Alan Chu <alan_chu@yahoo.com>
2019-11-15 17:45:31 +1100

    Updates

---
+++ precache-manifest.d234a3b9e678fb5b86597658f089115b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa5bb1d8ffe4afbfca85b4dbf5316e07",
+    "url": "/kiwiscout/index.html"
+  },
+  {
+    "revision": "cacc776a3e6dc45def37",
+    "url": "/kiwiscout/static/css/2.cc034990.chunk.css"
+  },
+  {
+    "revision": "b883f03a55afe06d7491",
+    "url": "/kiwiscout/static/css/main.de9dee24.chunk.css"
+  },
+  {
+    "revision": "cacc776a3e6dc45def37",
+    "url": "/kiwiscout/static/js/2.68475a4a.chunk.js"
+  },
+  {
+    "revision": "b883f03a55afe06d7491",
+    "url": "/kiwiscout/static/js/main.4244d574.chunk.js"
+  },
+  {
+    "revision": "a93da686782beef21aa6",
+    "url": "/kiwiscout/static/js/runtime~main.cc02c62e.js"
+  }
+]);
